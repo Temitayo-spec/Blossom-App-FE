f35e8b6eb206bef4b3341f5dd7e3f7b8674e81b6
Temitayo-spec <olawanletemitayo@gmail.com>
2024-05-05 21:14:15 +0100

    feat: set up file import path

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
+import { Button } from '@/components';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello world!</Text>
+      <Button />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { View, Text } from 'react-native';
+
+const Button: FC<{}> = () => {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  );
+};
+
+export default Button;
